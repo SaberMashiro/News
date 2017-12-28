95d3939a5ce3dac453124a71988c108a15136085
Bruce He <xprtion@outlook.com>
2017-12-28 17:11:20 +0800

    Update table.sql
    Table structure updated

--- database/table.sql
+++ database/table.sql
@@ -1,38 +1,41 @@
 USE news;
 
 CREATE TABLE user (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(32) UNIQUE NOT NULL,
+    username VARCHAR(32) PRIMARY KEY,
     password VARCHAR(32) NOT NULL,
     priority TINYINT UNSIGNED NOT NULL
 );
 
 CREATE TABLE category (
-    cat_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(16) NOT NULL
+    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(16)
 );
 
 CREATE TABLE news (
     news_id INT AUTO_INCREMENT PRIMARY KEY,
-    cat_id INT,
-    user_id INT,
     title VARCHAR(128) NOT NULL,
-    title_img TEXT,
+    icon TEXT,
     synopsis TEXT,
     content TEXT NOT NULL,
-    attachment TEXT,
-    time TIMESTAMP,
+    time TIMESTAMP DEFAULT now(),
+    username VARCHAR(32),
     clicks INT,
-    CONSTRAINT fk_news_cat FOREIGN KEY(cat_id) REFERENCES category(cat_id) ON DELETE CASCADE,
-    CONSTRAINT fk_news_user FOREIGN KEY(user_id) REFERENCES user(user_id) ON DELETE CASCADE
+    CONSTRAINT fk_news_user FOREIGN KEY(username) REFERENCES user(username) ON DELETE CASCADE
+);
+
+CREATE TABLE category_news (
+    category_id INT,
+    news_id INT,
+    CONSTRAINT fk_category_news_category FOREIGN KEY(category_id) REFERENCES category(category_id) ON DELETE CASCADE,
+    CONSTRAINT fk_category_news_news FOREIGN KEY(news_id) REFERENCES news(news_id) ON DELETE CASCADE
 );
 
 CREATE TABLE comment (
     comment_id INT AUTO_INCREMENT PRIMARY KEY,
     news_id INT,
+    guestname VARCHAR(32) DEFAULT '匿名',
     content TEXT NOT NULL,
-    time TIMESTAMP,
-    state TINYINT,
+    time TIMESTAMP DEFAULT now(),
     ip VARCHAR(32),
     CONSTRAINT fk_review_news FOREIGN KEY(news_id) REFERENCES news(news_id) ON DELETE CASCADE
 );
